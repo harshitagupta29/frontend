57c3e5dbec0ba3a2ad7e3e49205ad8c3b2522cbb
Harshita Gupta <harshitagupta2906@gmail.com>
2025-07-18 22:08:00 +0530

    Fix: Corrected hero image file name

--- src/app/page.js
+++ src/app/page.js
@@ -35,7 +35,7 @@ export default function Home() {
             
           </div>
           <div>
-            <Image src="/captureit.png" alt="Hero" width={500} height={400} className="w-full h-auto" />
+            <Image src="/Captureit.PNG" alt="Hero" width={500} height={400} className="w-full h-auto" />
           </div>
         </div>
       </section>
